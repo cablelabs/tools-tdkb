579dae7950c8905a1cf6c8728152153cf9e6e813
svelusamy <sugasini@tataelxsi.co.in>
2016-10-16 22:32:01 +0000

    RDKCMF-4917 Fixing of Failures in WECB Scripts
    Reason for change: Handled the script failures in wecb
    Test Procedure: Execution of wecb test cases in XB3 from test manager
    Risks: None
    Signed-off-by: svelusamy <svelusamy@tataelxsi.co.in>
    
    Upstream SHA1: 8cc654bcb7062568b404941dad9ae5c2c084c37a
    
    Change-Id: I6859014aec6ab43e8520e215063f9e29097e80c7

--- testscripts/RDKB/component/WECB/TS_WECB_Set_Radio_Update.py
+++ testscripts/RDKB/component/WECB/TS_WECB_Set_Radio_Update.py
@@ -1,20 +1,18 @@
-##
-# ============================================================================
-# COMCAST CONFIDENTIAL AND PROPRIETARY
-# ============================================================================
-# This file and its contents are the intellectual property of Comcast.  It may
-# not be used, copied, distributed or otherwise  disclosed in whole or in part
-# without the express written permission of Comcast.
-# ============================================================================
-# Copyright (c) 2016 Comcast. All rights reserved.
-# ============================================================================
-##
+#  ============================================================================
+#  COMCAST C O N F I D E N T I A L AND PROPRIETARY
+#  ============================================================================
+#  This file (and its contents) are the intellectual property of Comcast.  It may
+#  not be used, copied, distributed or otherwise  disclosed in whole or in part
+#  without the express written permission of Comcast.
+#  ============================================================================
+#  Copyright (c) 2014 Comcast. All rights reserved.
+#  ===========================================================================
 '''
 <?xml version='1.0' encoding='utf-8'?>
 <xml>
   <id></id>
   <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
-  <version>2</version>
+  <version>7</version>
   <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_WECB_Set_Radio_Update</name>
   <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
@@ -46,15 +44,16 @@
     <rdk_version>RDKB</rdk_version>
     <!--  -->
   </rdk_versions>
+  <script_tags />
 </xml>
 '''
-
+																		
 #use tdklib library,which provides a wrapper for tdk testcase script 
 import tdklib;
 import time;
 
 #Test component to be tested
-obj = tdklib.TDKScriptingLibrary("wecb","RDKB");
+obj = tdklib.TDKScriptingLibrary("wifiagent","RDKB");
 
 #IP and Port of box, No need to change,
 #This will be replaced with correspoing Box Ip and port while executing script
@@ -70,11 +69,10 @@ if "SUCCESS" in loadmodulestatus.upper():
         obj.setLoadModuleStatus("SUCCESS");
 
         #Enable moca interface
-        tdkTestObj = obj.createTestStep("WECB_SetParamValues");
+        tdkTestObj = obj.createTestStep("WIFIAgent_Set");
         tdkTestObj.addParameter("paramName","Device.MoCA.X_CISCO_COM_WiFi_Extender.X_CISCO_COM_Radio_Updated");
         tdkTestObj.addParameter("paramValue","true");
         tdkTestObj.addParameter("paramType","boolean");
-        tdkTestObj.addParameter("commit",1);
         expectedresult="SUCCESS";
         tdkTestObj.executeTestCase(expectedresult);
         actualresult = tdkTestObj.getResult();
@@ -88,33 +86,6 @@ if "SUCCESS" in loadmodulestatus.upper():
             print "ACTUAL RESULT 1: %s" %details;
             #Get the result of execution
             print "[TEST EXECUTION RESULT] : %s" %actualresult ;
-            
-            #Disable Moca interface
-            tdkTestObj = obj.createTestStep("WECB_SetParamValues");
-            tdkTestObj.addParameter("paramName","Device.MoCA.X_CISCO_COM_WiFi_Extender.X_CISCO_COM_Radio_Updated");
-            tdkTestObj.addParameter("paramValue","false");
-            tdkTestObj.addParameter("paramType","boolean");
-            tdkTestObj.addParameter("commit",1);
-            expectedresult="SUCCESS";
-            tdkTestObj.executeTestCase(expectedresult);
-            actualresult = tdkTestObj.getResult();
-
-            if expectedresult in actualresult:
-                #Set the result status of execution
-                tdkTestObj.setResultStatus("SUCCESS");
-                details = tdkTestObj.getResultDetails();
-                print "TEST STEP 2: Disable the Radio update";
-                print "EXPECTED RESULT 2: Should disable the radio update";
-                print "ACTUAL RESULT 2: %s" %details;
-                #Get the result of execution
-                print "[TEST EXECUTION RESULT] : %s" %actualresult ; 
-            else:
-                tdkTestObj.setResultStatus("FAILURE");
-                details = tdkTestObj.getResultDetails();
-                print "TEST STEP 2: Disable the Radio update";
-                print "EXPECTED RESULT 2: Should disable the radio update";
-                print "ACTUAL RESULT 2: %s" %details;
-                print "[TEST EXECUTION RESULT] : %s" %actualresult ;
         else:
             tdkTestObj.setResultStatus("FAILURE");
             details = tdkTestObj.getResultDetails();
@@ -123,8 +94,14 @@ if "SUCCESS" in loadmodulestatus.upper():
             print "ACTUAL RESULT 1: %s" %details;
             print "[TEST EXECUTION RESULT] : %s" %actualresult ;              
             
-        obj.unloadModule("wecb");
+        obj.unloadModule("wifiagent");
 else:
         print "Failed to load WECB controller module";
         obj.setLoadModuleStatus("FAILURE");
         print "Module loading failed";
+
+					
+
+					
+
+					
