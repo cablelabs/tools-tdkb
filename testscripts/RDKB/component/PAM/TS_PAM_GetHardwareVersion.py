c4945a2f5eabc8f89c0f6c7636c0133624a8beb5
ssurendranpillai <smrithi_surendranpillai@comcast.com>
2016-10-21 13:49:19 +0000

    RDKCMF-5197 TDK coverage improvement of PAM module
    Reason for change: Adding tdk scripts for pam apis
    Test Procedure: Run pam scripts via TDK test manager
    Risks: None
    Signed-off-by: smrithis<smrithis@tataelxsi.co.in>
    
    Upstream SHA1: 7e45d88cfb23c1836212c4df18368223153e93ab
    
    Change-Id: I139e3c6d827e84b9969c8d8740d77bd9737e5e3a

---
+++ testscripts/RDKB/component/PAM/TS_PAM_GetHardwareVersion.py
@@ -0,0 +1,97 @@
+#  ============================================================================
+#  COMCAST C O N F I D E N T I A L AND PROPRIETARY
+#  ============================================================================
+#  This file (and its contents) are the intellectual property of Comcast.  It may
+#  not be used, copied, distributed or otherwise  disclosed in whole or in part
+#  without the express written permission of Comcast.
+#  ============================================================================
+#  Copyright (c) 2014 Comcast. All rights reserved.
+#  ===========================================================================
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>5</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TS_PAM_GetHardwareVersion</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>pam_GetParameterValues</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>tests the api which returns A string identifying the particular CPE model and version</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <script_tags />
+</xml>
+'''
+#import statement
+import tdklib; 
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("pam","RDKB");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_PAM_GetHardwareVersion');
+
+#Get the result of connection with test component and STB
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper():
+    #Set the result status of execution
+    obj.setLoadModuleStatus("SUCCESS");
+    tdkTestObj = obj.createTestStep('pam_GetParameterValues');
+    tdkTestObj.addParameter("ParamName","Device.DeviceInfo.HardwareVersion");
+    expectedresult="SUCCESS";
+
+    #Execute the test case in STB
+    tdkTestObj.executeTestCase("expectedresult");
+    actualresult = tdkTestObj.getResult();
+    details = tdkTestObj.getResultDetails();
+		
+    if expectedresult in actualresult:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1: Get the HardwareVersion";
+        print "EXPECTED RESULT 1: Should get the HardwareVersion";
+        print "ACTUAL RESULT 1: HardwareVersion is %s" %details;
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : SUCCESS, %s" %details;
+    else:
+        tdkTestObj.setResultStatus("FAILURE");	
+        print "TEST STEP 1: Get the HardwareVersion";
+        print "EXPECTED RESULT 1: Should get the HardwareVersion";
+        print "ACTUAL RESULT 1: Failure in getting the HardwareVersion. Details : %s" %details;
+        print "[TEST EXECUTION RESULT] : FAILURE";
+    obj.unloadModule("pam");
+   		 
+else:   
+        print "Failed to load pam module";
+        obj.setLoadModuleStatus("FAILURE");
+        print "Module loading failed";				
