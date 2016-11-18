d95e109b48945cb6e3762a6d4786c5b529a05e0e
svelusamy <sugasini@tataelxsi.co.in>
2016-11-18 21:51:44 +0000

    RDKCMF-5423 Removing Device.WiFi.Radio.1.X_CISCO_COM_ApplySettingSSID
    Reason for change: Using X_CISCO_COM_ApplySetting to restart WIFI changes
    Test Procedure: Run TS_WIFIAGENT_FactoryReset via TDK test manager
    Risks: None
    Signed-off-by: sugasini<sugasini@tataelxsi.co.in>
    
    Upstream SHA1: d02327738339268f3d9a42e2207cc26ba0712092
    
    Change-Id: I89a92e0ad7030eed37184a3542b91e02aae3fae9

--- WIFIAgent_stub/src/WIFIAgent.cpp
+++ WIFIAgent_stub/src/WIFIAgent.cpp
@@ -250,14 +250,13 @@ bool WIFIAgent::WIFIAgent_Set(IN const Json::Value& req, OUT Json::Value& respon
         DEBUG_PRINT(DEBUG_TRACE,"\n tdk_wifiagent_set --->Error in Set API Validation of WIFI Agent in DUT !!! \n");
     }
 
+    printf("Apply the wifi settings\n");
     if ((!strncmp(ParamName, "Device.WiFi.Radio.1.", 20)) || (!strncmp(ParamName, "Device.WiFi.AccessPoint.1.", 26)) || (!strncmp(ParamName, "Device.WiFi.SSID.1.", 19)))
     {
-        //returnValue = ssp_setParameterValue("Device.WiFi.Radio.1.X_CISCO_COM_ApplySettingSSID","1","int");
         retVal = ssp_setParameterValue("Device.WiFi.Radio.1.X_CISCO_COM_ApplySetting","true","boolean");
     }
     else if ((!strncmp(ParamName, "Device.WiFi.Radio.2.", 20)) || (!strncmp(ParamName, "Device.WiFi.AccessPoint.2.", 26)) || (!strncmp(ParamName, "Device.WiFi.SSID.2.", 19)))
     {
-        //returnValue = ssp_setParameterValue("Device.WiFi.Radio.2.X_CISCO_COM_ApplySettingSSID","2","int");
         retVal = ssp_setParameterValue("Device.WiFi.Radio.2.X_CISCO_COM_ApplySetting","true","boolean");
     }
 
