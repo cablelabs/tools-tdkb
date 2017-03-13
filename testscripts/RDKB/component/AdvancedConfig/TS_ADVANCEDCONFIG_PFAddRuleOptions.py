b95652c22b808cd68b866ae7d35079b630d64631
ejose <elizabeth.j@tataelxsi.co.in>
2017-03-13 09:03:32 +0000

    RDKCMF-6641 Adding RPI box type to AdvancedConfig scripts
    Reason for change: RPI box supports AdvancedConfig scripts
    Test Procedure: Run AdvancedConfig scripts via Test Manager
    Risks: None
    Signed-off-by: anju <anju.a@tataelxsi.co.in>
    
    Change-Id: I620074ef3e02a7140d4e1a42ab12db6cede9c229

--- testscripts/RDKB/component/AdvancedConfig/TS_ADVANCEDCONFIG_PFAddRuleOptions.py
+++ testscripts/RDKB/component/AdvancedConfig/TS_ADVANCEDCONFIG_PFAddRuleOptions.py
@@ -17,26 +17,45 @@
 # limitations under the License.
 ##########################################################################
 '''
-<?xml version="1.0" encoding="UTF-8"?><xml>
-  <id/>
-  <version>9</version>
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>12</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_ADVANCEDCONFIG_PFAddRuleOptions</name>
-  <primitive_test_id/>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
   <primitive_test_name>AdvancedConfig_Set</primitive_test_name>
-  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <primitive_test_version>2</primitive_test_version>
+  <!--  -->
   <status>FREE</status>
-  <synopsis/>
-  <groups_id/>
+  <!--  -->
+  <synopsis></synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
   <execution_time>1</execution_time>
+  <!--  -->
   <long_duration>false</long_duration>
-  <remarks/>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
   <skip>false</skip>
+  <!--  -->
   <box_types>
-    <box_type>Emulator</box_type>
     <box_type>Broadband</box_type>
+    <!--  -->
+    <box_type>Emulator</box_type>
+    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
+    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_ADVANCEDCONFIG_5</test_case_id>
@@ -101,14 +120,13 @@ TestManager GUI will publish the result as SUCCESS in Execution page</except_out
     <test_stub_interface>none</test_stub_interface>
     <test_script>TS_ADVANCEDCONFIG_PFAddRuleOptions</test_script>
     <skipped>No</skipped>
-    <release_version/>
-    <remarks/>
+    <release_version></release_version>
+    <remarks></remarks>
   </test_cases>
-  <script_tags/>
+  <script_tags />
 </xml>
-
 '''
-#use tdklib library,which provides a wrapper for tdk testcase script
+																		#use tdklib library,which provides a wrapper for tdk testcase script
 import tdklib;
 import tdkutility;
 
@@ -335,5 +353,11 @@ else:
         print "FAILURE to load Advancedconfig module";
         obj.setLoadModuleStatus("FAILURE");
         print "Module loading FAILURE";
+
+					
+
+					
+
+					
 
 					
--- testscripts/RDKB/component/AdvancedConfig/TS_ADVANCEDCONFIG_PTCheckStatusAfterEnable.py
+++ testscripts/RDKB/component/AdvancedConfig/TS_ADVANCEDCONFIG_PTCheckStatusAfterEnable.py
@@ -17,26 +17,45 @@
 # limitations under the License.
 ##########################################################################
 '''
-<?xml version="1.0" encoding="UTF-8"?><xml>
-  <id/>
-  <version>1</version>
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_ADVANCEDCONFIG_PTCheckStatusAfterEnable</name>
-  <primitive_test_id/>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
   <primitive_test_name>AdvancedConfig_Get</primitive_test_name>
+  <!--  -->
   <primitive_test_version>2</primitive_test_version>
+  <!--  -->
   <status>FREE</status>
+  <!--  -->
   <synopsis>To check the status of the rule after enabling it</synopsis>
-  <groups_id/>
+  <!--  -->
+  <groups_id />
+  <!--  -->
   <execution_time>1</execution_time>
+  <!--  -->
   <long_duration>false</long_duration>
-  <remarks/>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
   <skip>false</skip>
+  <!--  -->
   <box_types>
     <box_type>Broadband</box_type>
+    <!--  -->
     <box_type>Emulator</box_type>
+    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
+    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_ADVANCEDCONFIG_23</test_case_id>
@@ -91,13 +110,13 @@ TestManager GUI will publish the result as SUCCESS in Execution page</except_out
     <test_stub_interface>None</test_stub_interface>
     <test_script>TS_ADVANCEDCONFIG_PTCheckStatusAfterEnable</test_script>
     <skipped>No</skipped>
-    <release_version/>
-    <remarks/>
+    <release_version></release_version>
+    <remarks></remarks>
   </test_cases>
+  <script_tags />
 </xml>
-
 '''
-# use tdklib library,which provides a wrapper for tdk testcase script
+						# use tdklib library,which provides a wrapper for tdk testcase script
 import tdklib;
 
 #Test component to be tested
@@ -300,3 +319,5 @@ else:
     print "FAILURE to load Advancedconfig module";
     obj.setLoadModuleStatus("FAILURE");
     print "Module loading FAILURE";
+
+					
