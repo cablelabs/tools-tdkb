ce3b3b721e42646a2e8edb41af1fd14f84eadcf8
Jim Lawton <jim.lawton@accenture.com>
2017-03-16 12:28:31 +0000

    RDKCMF-6645 Removing RPI box type from PAM scripts
    Reason for change: Some of the PAM scripts are not applicable for RPI
    Test Procedure: Run PAM scripts via Test Manager
    Risks: None
    Signed-off-by: anju <anju.a@tataelxsi.co.in>
    
    Change-Id: I0acd0395aba3bd0306b6bff321d04994b7fc1c10

--- testscripts/RDKB/component/PAM/TS_PAM_SetCloudUIEnable.py
+++ testscripts/RDKB/component/PAM/TS_PAM_SetCloudUIEnable.py
@@ -17,26 +17,44 @@
 # limitations under the License.
 ##########################################################################
 '''
-<?xml version="1.0" encoding="UTF-8"?><xml>
-  <id/>
-  <version>5</version>
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>7</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_PAM_SetCloudUIEnable</name>
-  <primitive_test_id/>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
   <primitive_test_name>pam_SetParameterValues</primitive_test_name>
+  <!--  -->
   <primitive_test_version>2</primitive_test_version>
+  <!--  -->
   <status>FREE</status>
-  <synopsis/>
-  <groups_id/>
+  <!--  -->
+  <synopsis></synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
   <execution_time>15</execution_time>
+  <!--  -->
   <long_duration>false</long_duration>
-  <remarks/>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
   <skip>false</skip>
+  <!--  -->
   <box_types>
     <box_type>RPI</box_type>
     <box_type>Broadband</box_type>
+    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
+    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_PAM_37</test_case_id>
@@ -74,14 +92,13 @@ TestManager GUI will publish the result as PASS in Execution/Console page of Tes
     <test_stub_interface>None</test_stub_interface>
     <test_script>TS_PAM_SetCloudUIEnable</test_script>
     <skipped>No</skipped>
-    <release_version/>
-    <remarks/>
+    <release_version></release_version>
+    <remarks></remarks>
   </test_cases>
-  <script_tags/>
+  <script_tags />
 </xml>
-
 '''
-																								
+																																				
 #use tdk library											
 import tdklib; 
 
@@ -156,6 +173,10 @@ else:
 					
 
 							
+
+					
+
+					
 
 					
 
