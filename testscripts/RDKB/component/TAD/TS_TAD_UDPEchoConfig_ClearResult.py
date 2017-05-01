2f0206d4ed5847ea59513a0b0d1489d76daef11a
svelusamy <sugasini@tataelxsi.co.in>
2017-05-01 15:53:58 +0000

    RDKCMF-6941 Delay of 10 seconds required to clear UDPEchoConfig result
    Reason for change: Modified UDPEchoConfig_ClearResult to add delay
    				   between UDPEchoConfig enable/disable
    Test Procedure: Execute TS_TAD_UDPEchoConfig_ClearResult using TDK
    Risks: None
    Signed-off-by: svelusamy <svelusamy@tataelxsi.co.in>
    
    Change-Id: Ie78e683f20d619b8eaf73710a032745e8e2bc5ab

--- testscripts/RDKB/component/TAD/TS_TAD_UDPEchoConfig_ClearResult.py
+++ testscripts/RDKB/component/TAD/TS_TAD_UDPEchoConfig_ClearResult.py
@@ -17,28 +17,45 @@
 # limitations under the License.
 ##########################################################################
 '''
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <xml>
-  <id/>
-  <version>1</version>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_TAD_UDPEchoConfig_ClearResult</name>
-  <primitive_test_id/>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
   <primitive_test_name>TADstub_Set</primitive_test_name>
+  <!--  -->
   <primitive_test_version>1</primitive_test_version>
+  <!--  -->
   <status>FREE</status>
+  <!--  -->
   <synopsis>To check if the result details are getting cleared when UDPEchoConfig is enabled</synopsis>
-  <groups_id/>
-  <execution_time>1</execution_time>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
   <long_duration>false</long_duration>
+  <!--  -->
   <advanced_script>false</advanced_script>
-  <remarks/>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
   <skip>false</skip>
+  <!--  -->
   <box_types>
     <box_type>Broadband</box_type>
+    <!--  -->
     <box_type>Emulator</box_type>
+    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
+    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_TAD_44</test_case_id>
@@ -74,11 +91,11 @@ TestManager GUI will publish the result as PASS in Execution/Console page of Tes
     <test_stub_interface>None</test_stub_interface>
     <test_script>TS_TAD_UDPEchoConfig_ClearResult</test_script>
     <skipped>No</skipped>
-    <release_version/>
-    <remarks/>
+    <release_version></release_version>
+    <remarks></remarks>
   </test_cases>
+  <script_tags />
 </xml>
-
 '''
 # use tdklib library,which provides a wrapper for tdk testcase script
 import tdklib;
@@ -218,6 +235,7 @@ if "SUCCESS" in loadmodulestatus.upper() and sysloadmodulestatus.upper():
                                 print "ACTUAL RESULT 7: %s" %details;
                                 #Get the result of execution
                                 print "[TEST EXECUTION RESULT] : SUCCESS";
+                                time.sleep(10);
 				tdkTestObj = obj.createTestStep('TADstub_Set');
                 		tdkTestObj.addParameter("ParamName","Device.IP.Diagnostics.UDPEchoConfig.Enable");
                 		tdkTestObj.addParameter("ParamValue","true");
@@ -234,6 +252,7 @@ if "SUCCESS" in loadmodulestatus.upper() and sysloadmodulestatus.upper():
                 		    print "ACTUAL RESULT 8: %s" %details;
                 		    #Get the result of execution
                 		    print "[TEST EXECUTION RESULT] : SUCCESS";
+                                    time.sleep(10);
 				    tdkTestObj = obj.createTestStep('TADstub_Get');
                         	    tdkTestObj.addParameter("paramName","Device.IP.Diagnostics.UDPEchoConfig.BytesReceived");
                         	    expectedresult="SUCCESS";
