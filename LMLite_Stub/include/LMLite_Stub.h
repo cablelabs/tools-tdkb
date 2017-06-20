d9b73c4fdb9abd7432737d4f10e8f15b16799dae
ejose <elizabeth.j@tataelxsi.co.in>
2017-06-20 14:44:42 +0000

    RDKCMF-7269 TDKB-149 : TDK coverage improvement of LMLite module
    Reason for change: Adding new scripts in lmlite module
    Test Procedure: Run scripts via TDK test manager
    Risks: None
    Signed-off-by: Anju<anju.a@tataelxsi.co.in>
    
    Change-Id: I2152bf86be52d6344af9ee0b9516783255fb26be

---
+++ LMLite_Stub/include/LMLite_Stub.h
@@ -0,0 +1,60 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2016 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+#ifndef __LMLITE_STUB_H__
+#define __LMLITE_STUB_H__
+#include <json/json.h>
+#include <unistd.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include "rdkteststubintf.h"
+#include "rdktestagentintf.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <sstream>
+#define IN
+#define OUT
+
+#define TEST_SUCCESS true
+#define TEST_FAILURE false
+#define MAX_PARAM_SIZE  100
+#define MAX_PARAM_NAMES_ARRAY   1000
+
+class RDKTestAgent;
+
+class LMLiteStub : public RDKTestStubInterface
+{
+    public:
+        /*Ctor*/
+        LMLiteStub();
+
+        /*inherited functions*/
+        bool initialize(IN const char* szVersion, IN RDKTestAgent *ptrAgentObj);
+
+        bool cleanup(IN const char* szVersion,IN RDKTestAgent *ptrAgentObj);
+        std::string testmodulepre_requisites();
+        bool testmodulepost_requisites();
+        /*LMlite Stub Wrapper functions*/
+        bool LMLiteStub_Get(IN const Json::Value& req, OUT Json::Value& response);
+        bool LMLiteStub_Set(IN const Json::Value& req, OUT Json::Value& response);
+   
+
+};
+#endif //__LMLITE_STUB_H__
