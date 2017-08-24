9274639b5d1fd86337f1b802a1161fa3faabd6c6
Elizabeth <Elizabeth_Jose@comcast.com>
2017-08-24 16:04:59 +0000

    RDKCMF-7656 TDKB-189 : POC on HAL redesign
    Reason for change: Modifying hal scripts as per the re-factored stub
    Test Procedure: Run wifi hal and cm hal scripts via TDK test manager
    Risks: None
    Signed-off-by: Smrithi<smrithis@tataelxsi.co.in>
    
    Change-Id: I18ff132088a38e1b110be065a270a6d6fa77a282

---
+++ testscripts/RDKB/component/CMHAL/TS_CMHAL_GetUpStreamDataRate.py
@@ -0,0 +1,134 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>3</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>TS_CMHAL_GetUpStreamDataRate</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>CMHAL_GetParamCharValue</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To get the upstream data rate and validate it. It should be within 3 Mbit/s to 25 Mbit/s</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Broadband</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_COSACM_77</test_case_id>
+    <test_objective>To get the upstream data rate and validate it. It should be within 3 Mbit/s to 25 Mbit/s</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband</test_setup>
+    <pre_requisite>1.Ccsp Components  should be in a running state of DUT that includes component under test Cable Modem
+2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
+    <api_or_interface_used>docsis_GetDOCSISInfo</api_or_interface_used>
+    <input_parameters>paramName : "US_DataRate"</input_parameters>
+    <automation_approch>1. Load  cmhal module
+2. From script invoke CMHAL_GetParamCharValue() 
+3. Get the Upstream data rate
+4. Validation of  the result is done within the python script and send the result status to Test Manager.
+5.Test Manager will publish the result in GUI as PASS/FAILURE based on the response from TAD stub.</automation_approch>
+    <except_output>The upstream data rate should be within 3Mbps to 25 Mbps</except_output>
+    <priority>High</priority>
+    <test_stub_interface>CosaCM</test_stub_interface>
+    <test_script>TS_CMHAL_GetUpStreamDataRate</test_script>
+    <skipped>No</skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("cmhal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_CMHAL_GetUpStreamDataRate');
+
+#Get the result of connection with test component and DUT
+loadmodulestatus =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
+
+if "SUCCESS" in loadmodulestatus.upper():
+    obj.setLoadModuleStatus("SUCCESS");
+
+    #Script to load the configuration file of the component
+    tdkTestObj = obj.createTestStep("CMHAL_GetParamCharValue");
+    tdkTestObj.addParameter("paramName","US_DataRate");
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    dataRate = tdkTestObj.getResultDetails();
+    dataRate = dataRate.split(" ")[0];
+
+    #convert dataRate from bps to mbps
+    dataRate = float(dataRate)/1000000;
+    Min_dataRate = 3;
+    Max_dataRate = 25;
+    if expectedresult in actualresult and Min_dataRate <= dataRate <= Max_dataRate:
+        #Set the result status of execution
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "TEST STEP 1: Get the Upstream data rate";
+        print "EXPECTED RESULT 1: Should get the Upstream datarate successfully";
+        print "ACTUAL RESULT 1: Upstream data rate is ",dataRate,"Mbps";
+        #Get the result of execution
+        print "[TEST EXECUTION RESULT] : SUCCESS";
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "TEST STEP 1: Get the Upstream data rate";
+        print "EXPECTED RESULT 1: Should get the Upstream data rate successfully";
+        print "ACTUAL RESULT 1: Failed to get the downstream data rate";
+        print "[TEST EXECUTION RESULT] : FAILURE";
+
+    obj.unloadModule("cmhal");
+else:
+        print "Failed to load the module";
+        obj.setLoadModuleStatus("FAILURE");
+        print "Module loading failed";
