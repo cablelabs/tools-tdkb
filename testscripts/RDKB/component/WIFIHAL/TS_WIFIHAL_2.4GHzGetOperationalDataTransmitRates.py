4536653a0e1829ac4a279641b044d0979cd476dd
Ranjith <Ranjith_Pallakkatt@comcast.com>
2017-12-26 12:35:34 +0000

    TDKB-225:WiFi Hal Validation
    Reason for change: Adding new scripts for WIFIHAL validation
    Test Procedure: Test the scripts using TM
    Risks: None
    Signed-off-by: Nousha Surendran<nousha.s@tataelxsi.co.in>
    
    Change-Id: I6fb813e131490998fbb578e14a1949069a56e447

--- testscripts/RDKB/component/WIFIHAL/TS_WIFIHAL_2.4GHzGetOperationalDataTransmitRates.py
+++ testscripts/RDKB/component/WIFIHAL/TS_WIFIHAL_2.4GHzGetOperationalDataTransmitRates.py
@@ -19,11 +19,11 @@
 '''
 <?xml version="1.0" encoding="UTF-8"?><xml>
   <id/>
-  <version>8</version>
-  <name>TS_WIFIHAL_5GHzGetOperationalDataTransmitRates</name>
+  <version>1</version>
+  <name>TS_WIFIHAL_2.4GHzGetOperationalDataTransmitRates</name>
   <primitive_test_id/>
   <primitive_test_name>WIFIHAL_GetOrSetParamStringValue</primitive_test_name>
-  <primitive_test_version>1</primitive_test_version>
+  <primitive_test_version>8</primitive_test_version>
   <status>FREE</status>
   <synopsis>Get the operational data transmit rate using wifi_getRadioOperationalDataTransmitRates() and see if its a subset of values returned by wifi_getRadioSupportedDataTransmitRates()</synopsis>
   <groups_id/>
@@ -40,30 +40,29 @@
     <rdk_version>RDKB</rdk_version>
   </rdk_versions>
   <test_cases>
-    <test_case_id>TC_WIFIHAL_05</test_case_id>
+    <test_case_id>TC_WIFIHAL_119</test_case_id>
     <test_objective>Get the operational data transmit rate using wifi_getRadioOperationalDataTransmitRates() and see if its a subset of values returned by wifi_getRadioSupportedDataTransmitRates()</test_objective>
     <test_type>Positive</test_type>
-    <test_setup>XB3. XB6, Rpi</test_setup>
+    <test_setup>Broadband, RPI</test_setup>
     <pre_requisite>1.Ccsp Components  should be in a running state else invoke cosa_start.sh manually that includes all the ccsp components and TDK Component
 2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
     <api_or_interface_used>wifi_getRadioOperationalDataTransmitRates()
 wifi_getRadioSupportedDataTransmitRates()</api_or_interface_used>
     <input_parameters>methodName : getSupportedDataTransmitRates
 methodName : getOperationalDataTransmitRates
-radioIndex : 1</input_parameters>
+radioIndex : 0</input_parameters>
     <automation_approch>1. Load wifihal module
 2. Invoke wifi_getRadioOperationalDataTransmitRates()  to get the current data transmit rate
 3.Get the supported transmit rate using wifi_getRadioSupportedDataTransmitRates()  and check if current value is from this list
-5. Unload wifihal module</automation_approch>
+4. Unload wifihal module</automation_approch>
     <except_output>OperationalDataTransmitRates should be a subset of supported data transmit rates</except_output>
     <priority>High</priority>
     <test_stub_interface>WIFIHAL</test_stub_interface>
-    <test_script>TS_WIFIHAL_5GHzGetOperationalDataTransmitRates</test_script>
+    <test_script>TS_WIFIHAL_2.4GHzGetOperationalDataTransmitRates</test_script>
     <skipped>No</skipped>
     <release_version/>
     <remarks/>
   </test_cases>
-  <script_tags/>
 </xml>
 
 '''
@@ -78,7 +77,7 @@ obj = tdklib.TDKScriptingLibrary("wifihal","1");
 #This will be replaced with correspoing Box Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'TS_WIFIHAL_5GHzGetOperationalDataTransmitRates');
+obj.configureTestCase(ip,port,'TS_WIFIHAL_2.4GHzGetOperationalDataTransmitRates');
 
 loadmodulestatus =obj.getLoadModuleResult();
 print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus
@@ -87,10 +86,9 @@ if "SUCCESS" in loadmodulestatus.upper():
     obj.setLoadModuleStatus("SUCCESS");
 
     expectedresult="SUCCESS";
-    radioIndex = 1
+    radioIndex = 0
     flag = 1
     getMethod = "getSupportedDataTransmitRates"
-    #getMethod = "getRadioSupportedFrequencyBands"
     primitive = "WIFIHAL_GetOrSetParamStringValue"
     tdkTestObj, actualresult, details = ExecuteWIFIHalCallMethod(obj, primitive, radioIndex, "0", getMethod)
 
@@ -102,11 +100,11 @@ if "SUCCESS" in loadmodulestatus.upper():
         if expectedresult in actualresult :
             operationalRates = details.split(":")[1].strip().split(",")
 
-	    for rate in operationalRates:
-		if rate not in supportedRates:
-		    flag = 0;
-		    break;
-	    if flag == 1:
+            for rate in operationalRates:
+                if rate not in supportedRates:
+                    flag = 0;
+                    break;
+            if flag == 1:
                 print "OperationalDataTransmitRates is in the list of SupportedDataTransmitRates"
                 tdkTestObj.setResultStatus("SUCCESS");
             else:
