4e7b6d3e53474cb0d74a0f38b152882682087acc
Ranjith <Ranjith_Pallakkatt@comcast.com>
2018-01-30 06:58:50 +0000

    TDKB-233:WiFi Hal Validation
    Reason for change: Not applicable scripts
    Test Procedure: Test the scripts using TM
    Risks: None
    Signed-off-by: Nousha Surendran<nousha.s@tataelxsi.co.in>
    
    Change-Id: Ia2324665e39da810306502956771251285f3e713

--- testscripts/RDKB/component/WIFIHAL/TS_WIFIHAL_5GHzSetRadioDCSChannelPool.py
+++ testscripts/RDKB/component/WIFIHAL/TS_WIFIHAL_5GHzSetRadioDCSChannelPool.py
@@ -34,7 +34,6 @@
   <skip>false</skip>
   <box_types>
     <box_type>Broadband</box_type>
-    <box_type>RPI</box_type>
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
@@ -43,7 +42,7 @@
     <test_case_id>TC_WIFIHAL_126</test_case_id>
     <test_objective>To set the dynamic channel selection pool from a set of possible channels and verify by getting it</test_objective>
     <test_type>Positive</test_type>
-    <test_setup>Broadband, RPI</test_setup>
+    <test_setup>Broadband</test_setup>
     <pre_requisite>1.Ccsp Components  should be in a running state else invoke cosa_start.sh manually that includes all the ccsp components and TDK Component
 2.TDK Agent should be in running state or invoke it through StartTdk.sh script</pre_requisite>
     <api_or_interface_used>wifi_getRadioPossibleChannels()
