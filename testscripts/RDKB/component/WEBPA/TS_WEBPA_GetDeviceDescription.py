8d9e7b4e2ada335f3cb37544f9377c1c6460f432
sarveswaran <Smrithi_Surendran@comcast.com>
2018-04-24 14:20:37 +0000

    TDKB-425 : WEBPA component testing
    Reason for change: Adding test scripts and xml for webpa testing
    Test Procedure: Run WEBPA scripts via TDK TM
    Risks: None
    Signed-off-by:Smrithi<smrithis@tataelxsi.co.in>
    
    Change-Id: Ia743e87a14f8a290fbf0737a4a8ef5c13b60d82b

---
+++ testscripts/RDKB/component/WEBPA/TS_WEBPA_GetDeviceDescription.py
@@ -0,0 +1,106 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2018 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>TS_WEBPA_GetDeviceDescription</name>
+  <primitive_test_id/>
+  <primitive_test_name>WEBPA_Donothing</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>Use webpa to get the device info description of the gateway</synopsis>
+  <groups_id/>
+  <execution_time>5</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>Broadband</box_type>
+    <box_type>RPI</box_type>
+    <box_type>RDKB-Gateway</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDKB</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>TC_WEBPA_3</test_case_id>
+    <test_objective>Use webpa to get the device info description of the gateway</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Broadband,RPI,Emulator</test_setup>
+    <pre_requisite>If SAT token is to be used, token should be created and made available in test manager. Also the config variables SAT_REQUIRED, SAT_TOKEN_FILE, SERVER_URI should be updated in webpaVariables.py</pre_requisite>
+    <api_or_interface_used>webpaQuery
+parseWebpaResponse</api_or_interface_used>
+    <input_parameters>Device.DeviceInfo.Description</input_parameters>
+    <automation_approch>1. Load sysutil module
+2. Configure WEBPA server to send get request for Device.DeviceInfo.Description
+3. Parse the WEBPA response
+4.  If response status is 200 get operation was success otherwise failure
+5. Unload sysutil module</automation_approch>
+    <except_output>WEBPA response status should be 200</except_output>
+    <priority>High</priority>
+    <test_stub_interface>sysutil</test_stub_interface>
+    <test_script>TS_WEBPA_GetDeviceDescription</test_script>
+    <skipped>No</skipped>
+    <release_version>M58</release_version>
+    <remarks>None</remarks>
+  </test_cases>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from webpaUtility import *
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("sysutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'TS_WEBPA_GetDeviceDescription');
+
+#Get the result of connection with test component and STB
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+
+if "SUCCESS" in result.upper() :
+    #Set the module loading status
+    obj.setLoadModuleStatus("SUCCESS");
+
+    queryParam = {"name":"Device.DeviceInfo.Description"}
+    queryResponse = webpaQuery(obj, queryParam)
+
+    parsedResponse = parseWebpaResponse(queryResponse)
+    tdkTestObj = obj.createTestStep('ExecuteCmd');
+    tdkTestObj.executeTestCase("SUCCESS");
+    if 200 in parsedResponse:
+        tdkTestObj.setResultStatus("SUCCESS");
+        print "[TEST EXECUTION RESULT] : SUCCESS"
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "[TEST EXECUTION RESULT] : FAILURE"
+    obj.unloadModule("sysutil");
+
+else:
+    print "FAILURE to load module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading FAILURE";   
