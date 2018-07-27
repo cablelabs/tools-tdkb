80f96c909350d895fddac0145bc75c24cdfb5c55
rpallakkatt <Ranjith_Pallakkatt@comcast.com>
2018-07-27 12:40:39 +0000

    TDKB-553: Script Modification DHCP module
    Reason for change: For handling Null value check.
    Test Procedure:Run WIFIHAL test case on DUT
    Risks: None
    Signed-off-by: Katha Akhila Reddy <katha.r@tataelxsi.co.in>
    
    Change-Id: I0c69797e7f6d070cc2a0e360eed5d1725a83ed7a

--- testscripts/RDKB/component/HAL_DHCP/TS_dhcp_stub_hal_get_ecm_dns_svrs.py
+++ testscripts/RDKB/component/HAL_DHCP/TS_dhcp_stub_hal_get_ecm_dns_svrs.py
@@ -1,40 +1,59 @@
-#  ===========================================================================
-#  Copyright 2016-2017 Intel Corporation
-
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-
-#http://www.apache.org/licenses/LICENSE-2.0
-
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
-#  ===========================================================================
-
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2018 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
 '''
-<?xml version="1.0" encoding="UTF-8"?><xml>
-  <id/>
-  <version>9</version>
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>12</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>TS_dhcp_stub_hal_get_ecm_dns_svrs</name>
-  <primitive_test_id></primitive_test_id>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
   <primitive_test_name>dhcp_stub_hal_get_ecm_dns_svrs</primitive_test_name>
-  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <primitive_test_version>2</primitive_test_version>
+  <!--  -->
   <status>FREE</status>
+  <!--  -->
   <synopsis>To validate HAL API dhcp_stub_hal_get_ecm_dns_svrs()</synopsis>
-  <groups_id/>
+  <!--  -->
+  <groups_id />
+  <!--  -->
   <execution_time>1</execution_time>
+  <!--  -->
   <long_duration>false</long_duration>
+  <!--  -->
   <advanced_script>false</advanced_script>
-  <remarks/>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
   <skip>false</skip>
+  <!--  -->
   <box_types>
     <box_type>Broadband</box_type>
+    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDKB</rdk_version>
+    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_HAL_DHCP_3</test_case_id>
@@ -60,7 +79,6 @@
   </test_cases>
   <script_tags />
 </xml>
-
 '''
 #Library function
 import tdklib;
@@ -81,35 +99,44 @@ loadmodulestatus =obj.getLoadModuleResult();
 print "[LIB LOAD STATUS]  :  %s" %loadmodulestatus ;
 
 if "SUCCESS" in loadmodulestatus.upper():
-        obj.setLoadModuleStatus("SUCCESS");
+    obj.setLoadModuleStatus("SUCCESS");
 
-        #Script to load the configuration file of the component
-        tdkTestObj = obj.createTestStep("dhcp_stub_hal_get_ecm_dns_svrs");
-        expectedresult="SUCCESS";
-        tdkTestObj.executeTestCase(expectedresult);
-        actualresult = tdkTestObj.getResult();
+    #Script to load the configuration file of the component
+    tdkTestObj = obj.createTestStep("dhcp_stub_hal_get_ecm_dns_svrs");
+    expectedresult="SUCCESS";
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
         
-        details = tdkTestObj.getResultDetails();
-        details1 = details.split("/")[1];
-        if expectedresult in actualresult and (int (details1.split(".")[0]) > 0):
-            #Set the result status of execution
-            tdkTestObj.setResultStatus("SUCCESS");
-            print "TEST STEP 1: Retrieve the dhcp_stub_hal_get_ecm_dns_svrs";
-            print "EXPECTED RESULT 1: Should retrieve the dhcp_stub_hal_get_ecm_dns_svrssuccessfully";
-            print "ACTUAL RESULT 1: %s" %details;
-            print "pList->number: %s" %details.split("/")[2];
-            print "pList->addrs: %s" %details.split("/")[1];
-            #Get the result of execution
-            print "[TEST EXECUTION RESULT] : %s" %actualresult;
+    details = tdkTestObj.getResultDetails();
+    print "details : %s" %details
+       
+    if expectedresult in actualresult:
+        if details !="":
+            details1 = details.split("/")[1];
+            if (int (details1.split(".")[0]) > 0):
+                #Set the result status of execution
+                tdkTestObj.setResultStatus("SUCCESS");
+                print "TEST STEP 1: Retrieve the dhcp_stub_hal_get_ecm_dns_svrs";
+                print "EXPECTED RESULT 1: Should retrieve the dhcp_stub_hal_get_ecm_dns_svrssuccessfully";
+                print "ACTUAL RESULT 1: %s" %details;
+                print "pList->number: %s" %details.split("/")[2];
+                print "pList->addrs: %s" %details.split("/")[1];
+                #Get the result of execution
+                print "[TEST EXECUTION RESULT] : %s" %actualresult;
+            else:
+                tdkTestObj.setResultStatus("FAILURE");
+                print "TEST STEP 1: Retrieve the dhcp_stub_hal_get_ecm_dns_svrs";
+                print "EXPECTED RESULT 1: Should retrieve the dhcp_stub_hal_get_ecm_dns_svrs successfully";
+                print "ACTUAL RESULT 1: %s" %details;
+                print "[TEST EXECUTION RESULT] : Failure";
         else:
             tdkTestObj.setResultStatus("FAILURE");
-            print "TEST STEP 1: Retrieve the dhcp_stub_hal_get_ecm_dns_svrs";
-            print "EXPECTED RESULT 1: Should retrieve the dhcp_stub_hal_get_ecm_dns_svrs successfully";
-            print "ACTUAL RESULT 1: %s" %details;
-            print "[TEST EXECUTION RESULT] : Failure";
-
-        obj.unloadModule("dhcp");
+            print "dhcp_stub_hal_get_ecm_dns_svrs() is returning NULL value";
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "dhcp_stub_hal_get_ecm_dns_svrs() operation failed";
+    obj.unloadModule("dhcp");
 else:
-        print "Failed to load the module";
-        obj.setLoadModuleStatus("FAILURE");
-        print "Module loading failed";
+    print "Failed to load the module";
+    obj.setLoadModuleStatus("FAILURE");
+    print "Module loading failed";
